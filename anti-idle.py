f838c36ecd666e8b680efcddeb97036740787c79
srrequiem <theanondebian@gmail.com>
2021-08-12 13:29:06 -0500

    keystroke event handled

--- anti-idle.py
+++ anti-idle.py
@@ -1,12 +1,14 @@
 import pyautogui
 from threading import Timer
 from time import sleep
+from pynput.keyboard import Listener
 
 width, height = pyautogui.size()
 middleWidth = width / 2
 middleHeight = height / 2
 leftXLimit = middleWidth / 2
 rightXLimit = middleWidth + leftXLimit
+
 class RepeatedTimer(object):
     def __init__(self, interval, function, *args, **kwargs):
         self._timer     = None
@@ -15,7 +17,6 @@ class RepeatedTimer(object):
         self.args       = args
         self.kwargs     = kwargs
         self.is_running = False
-        self.start()
 
     def _run(self):
         self.is_running = False
@@ -37,9 +38,19 @@ def moveMouse():
         pyautogui.moveTo(leftXLimit,middleHeight,duration=0.25)
         pyautogui.moveTo(rightXLimit,middleHeight,duration=0.25)
 
-def main():
+def isUserEmittingMovement(x, y):
+    return y != middleHeight or x < leftXLimit or x > rightXLimit
+
+moveMouseTimer = RepeatedTimer(14, moveMouse)
+
+def on_press(key):
+    if moveMouseTimer.is_running:
+        moveMouseTimer.stop()
+
+if __name__ == "__main__":
+    keyboardListener = Listener(on_press=on_press)
+    keyboardListener.start()
     print('Press Ctrl-C to quit.')
-    moveMouseTimer = None
     try:
         while True:
             lastXPos = pyautogui.position().x
@@ -47,16 +58,12 @@ def main():
             sleep(1)
             x, y = pyautogui.position()
             if lastXPos == x and lastYPos == y:
-                if moveMouseTimer is None:
-                    moveMouseTimer = RepeatedTimer(14, moveMouse) # Timer creation
-                elif not moveMouseTimer.is_running:
+                if not moveMouseTimer.is_running:
                     moveMouseTimer.start() # Starting Timer
             elif moveMouseTimer.is_running:
-                if y != middleHeight or x < leftXLimit or x > rightXLimit:
+                if isUserEmittingMovement(x, y):
                     moveMouseTimer.stop() # Stopping Timer
     except KeyboardInterrupt:
         moveMouseTimer.stop()
+        keyboardListener.stop()
         print('\n')
-
-if __name__ == "__main__":
-    main()
